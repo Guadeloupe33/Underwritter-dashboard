a047e0748185e18d8e301f7be1d37140c2fe2ff6
Guadeloupe33 <stevenfrancius@gmail.com>
2025-07-13 18:00:28 -0400

    added scripts to the project

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleDetails(row) {
+    const nextRow = row.nextElementSibling;
+    if (nextRow && nextRow.classList.contains('details-row')) {
+      nextRow.style.display = nextRow.style.display === 'table-row' ? 'none' : 'table-row';
+    }
+  }
